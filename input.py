0da1ae2d7bb40dbec4d3b363e477c1c7680b089f
Kiran <59613353+bishtkiran@users.noreply.github.com>
2025-11-21 21:57:37 +0530

    Refactor : Update questions.

--- input.py
+++ input.py
@@ -1,9 +1,15 @@
+print("***************************************************************************************************************")
+
+print("Write a program that asks the user for their name and then prints a greeting   message using their name.")
+
 name = input("Enter you name. ")
 greeting = 'Good Morning'
 
 print(greeting + ', ' + name)
 
-print("***********************")
+print("***************************************************************************************************************")
+
+print("Ask the user to enter two numbers from the user and print their sum, multiplication, and division.")
 
 num_1 = int(input("Enter the first number "))
 
@@ -15,7 +21,9 @@ print("Product = " + str(num_1 * num_2))
 
 print("Division = " + str((num_1 / num_2)))
 
-print("***********************")
+print("***************************************************************************************************************")
+
+print("Ask the user to enter input names separated by commas, split the string from comma and copy to a list and print.")
 
 names = input("Enter comma separated names ")
 
@@ -23,7 +31,9 @@ list = names.split(",")
 
 print(list)
 
-print("***********************")
+print("***************************************************************************************************************")
+
+print("Ask the user to enter their age and check if they are eligible to vote based on their age.")
 
 age = int(input("Enter you age. "))
 
@@ -32,10 +42,12 @@ if age >= 18 :
 else :
     print("You are not eligible to vote")
 
-print("***********************")
+print("***************************************************************************************************************")
+
+print("For value = 3.14159, Using f-string print output for only up to 2 decimal places.")
 
 val = 3.14159
 
 print(f"{val:.2f}")
 
-print("***********************")
+print("***************************************************************************************************************")
