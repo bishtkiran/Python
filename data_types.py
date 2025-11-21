6155082499f06a9560555f85c494d4e2538c4858
Kiran <59613353+bishtkiran@users.noreply.github.com>
2025-11-21 21:52:39 +0530

    Refactor : update questions

--- data_types.py
+++ data_types.py
@@ -1,30 +1,41 @@
-print("*********************")
+print("*********************************************************")
+
+print("Convert 3.75 to an integer and print the value")
 
 num_1 = 3.75
 print(int(num_1))
 
-print("*********************")
+print("*********************************************************")
+
+print("Convert \"123\" to a float and print the value")
 
 num_2 = 123
 print(float(num_2))
 
-print("*********************")
+print("*********************************************************")
+
+print("Convert 0 to a boolean and print the value")
 
 num_3 = 0
 print(bool(num_3))
 
-print("*********************")
+print("*********************************************************")
 
+print("Convert False to a string and print the value")
 
 bool = False
 print(str(bool))
 
-print("*********************")
+print("*********************************************************")
+
+print("Convert all characters in the string to uppercase. x = \"hello\"")
 
 x = "hello"
 print(x.upper())
 
-print("*********************")
+print("*********************************************************")
+
+print("Given x = 5 and y = 3.14, calculate z = x + y and determine the data type of z. And convert it to integer.")
 
 x = 5
 y = 3.14
@@ -32,18 +43,29 @@ z = x + y
 print(type(z))
 print(int(z))
 
-print("*********************")
+print("*********************************************************")
+
+print("Convert the string to uppercase.")
 
 s= 'hello'
 print(s.upper())
 
-print("*********************")
+print("*********************************************************")
+
+print("Replace 'e' with 'a'.")
+
 print(s.replace('e','a'))
 
-print("*********************")
+print("*********************************************************")
+
+print("Check if the string starts with 'he'.")
+
 print(s.startswith('he'))
 
-print("*********************")
+print("*********************************************************")
+
+print("Check if the string ends with 'lo'.")
+
 print(s.endswith('lo'))
 
-print("*********************")
+print("*********************************************************")
