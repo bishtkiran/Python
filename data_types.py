251f9b68d68693f0cc5dc101f7db142be5dfbde6
Kiran <59613353+bishtkiran@users.noreply.github.com>
2025-11-21 19:56:00 +0530

    Add : add assignment for data types

---
+++ data_types.py
@@ -0,0 +1,49 @@
+print("*********************")
+
+num_1 = 3.75
+print(int(num_1))
+
+print("*********************")
+
+num_2 = 123
+print(float(num_2))
+
+print("*********************")
+
+num_3 = 0
+print(bool(num_3))
+
+print("*********************")
+
+
+bool = False
+print(str(bool))
+
+print("*********************")
+
+x = "hello"
+print(x.upper())
+
+print("*********************")
+
+x = 5
+y = 3.14
+z = x + y
+print(type(z))
+print(int(z))
+
+print("*********************")
+
+s= 'hello'
+print(s.upper())
+
+print("*********************")
+print(s.replace('e','a'))
+
+print("*********************")
+print(s.startswith('he'))
+
+print("*********************")
+print(s.endswith('lo'))
+
+print("*********************")
