5a6b47a8dee149d73f017997d54352781dac6f3a
Kiran <59613353+bishtkiran@users.noreply.github.com>
2025-11-22 13:11:17 +0530

    Add : add file calculate_area

---
+++ calculate_area.py
@@ -0,0 +1,9 @@
+print("Define a function calculate_area that calculates the area of a rectangle and return the result. If no width is provided, it defaults to 10.")
+
+def calculate_area(length, width = 10) :
+    return  length * width
+
+# area = calculate_area(10, 5)
+area = calculate_area(7)
+print(area)
+
