ee50dc15d7c94144c6fbad7c8d6bb1610b57c9be
Kiran <59613353+bishtkiran@users.noreply.github.com>
2025-11-30 19:27:13 +0530

    Add :: add assignment to write contents to a file

---
+++ WriteContent.py
@@ -0,0 +1,8 @@
+print("***************************************************************************************************************")
+
+print("Create a program to write the string “Hello, Python!” into a file named output.txt.")
+
+with open("output.txt", "w") as file:
+    file.write("Hello, Python!")
+
+print("***************************************************************************************************************")
